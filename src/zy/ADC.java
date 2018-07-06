3cf9ae9083e545023896d551ab68de36360d0d66
Your Name <you@example.com>
2018-07-06 13:03:56 +0800

    新分支中的新功能adc.java

---
+++ src/zy/ADC.java
@@ -0,0 +1,10 @@
+package zy;
+
+/**
+ * Created by zuoyao on 2018/7/6.
+ */
+public class ADC {
+    public static void main(String[] args) {
+        System.out.println("这是在新分支newbranch中开发的新功能");
+    }
+}
